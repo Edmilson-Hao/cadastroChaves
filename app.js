fddc8330577ff275fb7677a6ea0b3ac7e675ba81
Edmilson <45573538+Edmilson-Hao@users.noreply.github.com>
2022-08-04 13:28:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
+/*-------------------------------------------Variable declaration-------------------------------------------*/
 const chavesList = document.querySelector('[data-js="chaves-list"]')
 const db = firebase.firestore()
 const botaoEnviar = document.getElementById('botaoEnviar')
+const divCadastro = document.getElementById('divCadastro')
+const divPesquisarChave = document.getElementById('divPesquisarChave')
+const firstLabel = document.getElementById('firstLabel')
+const secondLabel = document.getElementById('secondLabel')
+const searchButton = document.getElementById('searchButton')
 let imobiliaria, vistoriador, endereço, tipo, observacao
+/*----------------------------------------------------------------------------------------------------------*/
 
+
+
+
+
+/*------------------------------------Write Form data to Variables------------------------------------------*/
 getFormData = () => {
-    //imobiliaria = document.getElementById('imobiliaria').value
     imobiliaria = document.getElementsByName('imobiliaria')[0].value
     vistoriador = document.getElementsByName('vistoriador')[0].value
     endereço = document.getElementById('endereco').value
     tipo = document.querySelector('input[name="tipodevistoria"]:checked').value
     observacao = document.getElementById('observacao').value
 }
+/*-----------------------------------Reset Form input information-------------------------------------------*/
 
+
+
+
+
+/*----------------------------------------------------------------------------------------------------------*/
 resetData = () => {
     document.getElementById('imobiliaria').value = ''
     document.getElementById('vistoriador').value = ''
@@ -20,29 +37,43 @@ resetData = () => {
     document.getElementById('observacao').value = ''
     document.getElementById('chaveFoto').value = ''
 }
-
-db.collection('chaves').get()
-	.then( snapshot => {
-		const chavesli = snapshot.docs.reduce((acc, doc) => {
-			acc += `
-				<li>${doc.data().Imobiliária}</li>
-				<li>${doc.data().Endereço}</li>
-				<li>${doc.data().Vistoriador}</li>
-				<li>${doc.data().Tipo}</li>
-				<li>${doc.data().Observação}</li>
-				<li><img src="${doc.data().Foto}"></li>
-				`
-			return acc
-		}, '')
-
-		chavesList.innerHTML = chavesli
-
-	})
-	.catch(err => {
-		console.log(err.message)
-	})
+/*----------------------------------------------------------------------------------------------------------*/
 
 
+
+
+
+/*--------------------------------------Send data to firebase-----------------------------------------------*/
+const getDataFromFirebase = () => {
+	db.collection('chaves').get()
+		.then( snapshot => {
+			const chavesli = snapshot.docs.reduce((acc, doc) => {
+				acc += `
+					<li>${doc.data().Imobiliária}</li>
+					<li>${doc.data().Endereço}</li>
+					<li>${doc.data().Vistoriador}</li>
+					<li>${doc.data().Tipo}</li>
+					<li>${doc.data().Observação}</li>
+					<li><img src="${doc.data().Foto}"></li>
+					`
+				return acc
+			}, '')
+
+			chavesList.innerHTML = chavesli
+
+		})
+		.catch(err => {
+			console.log(err.message)
+		}
+	)
+}
+/*----------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+/*---------------------------------------Watch for Send Button event----------------------------------------*/
 botaoEnviar.addEventListener('click', e => { //enviar dados
 	getFormData()
 	converterImagem()
@@ -56,55 +87,85 @@ botaoEnviar.addEventListener('click', e => { //enviar dados
 	}).then(successSignal).catch(filedSignal)
 
 })
+/*----------------------------------------------------------------------------------------------------------*/
 
 
 
 
-function converterImagem() {
+
+/*------------------------------------Convert Canvas image to Base64----------------------------------------*/
+const converterImagem = () => {
 	const canvas = document.getElementById('imagemReduzida');
     const imagemReduzida = canvas.toDataURL();
     foto = imagemReduzida
 	console.log(foto)
 }
+/*----------------------------------------------------------------------------------------------------------*/
 
 
-/*
-function converterImagem() {
 
-	var receberArquivo = document.getElementById('chaveFoto').files
 
-	if(receberArquivo.length > 0) {
-		var carregarImagem = receberArquivo[0]
 
-		var lerArquivo = new FileReader()
-		
-		lerArquivo.onload = function(arquivoCarregado) {
-			var imagemBase64 = arquivoCarregado.target.result
-			foto = imagemBase64
-			console.log(foto)
-		}
-
-		lerArquivo.readAsDataURL(carregarImagem)
-		
-	}
-}
-*/
+/*---------------------------------Print Success of Fail Signal to screen-----------------------------------*/
 const successSignal = () => {
     Swal.fire({
         position: 'center',
         icon: 'success',
-        title: 'Your work has been saved',
+        title: 'Chave cadastrada com sucesso!',
         showConfirmButton: false,
         timer: 1500
     })
 }
-
 const filedSignal = () => {
     Swal.fire({
         position: 'center',
         icon: 'error',
-        title: 'Your work has not been saved',
+        title: 'Erro ao tentar cadastrar a chave!',
         showConfirmButton: false,
         timer: 1500
     })
 }
+/*----------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+/*---------------------------Watch for Buttom click to hide and show site sections--------------------------*/
+firstLabel.addEventListener('click', e => {
+    divCadastro.hidden = false
+    divPesquisarChave.hidden = true
+})
+
+secondLabel.addEventListener('click', e => {
+    divCadastro.hidden = true
+    divPesquisarChave.hidden = false
+})
+/*----------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+/*----------------------------------Get search string and what search button--------------------------------*/
+const getSearchString = e => {
+	const searchString = document.getElementById('searchBar').value
+	console.log(searchString)
+    
+    generateKeyList(searchString)
+}
+
+
+
+searchButton.addEventListener('click',  getSearchString)
+/*----------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+/*---------------------------------------Generate Key List HTML---------------------------------------------*/
+const generateKeyList = searchString => {
+	alert(searchString)
+}
+/*----------------------------------------------------------------------------------------------------------*/
